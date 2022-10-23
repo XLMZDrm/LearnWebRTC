a56e1f656210a230b40453001c57533fe34b10dd
drmerveem <nsovipgl@gmail.com>
2022-10-23 11:34:13 +0700

    done demo

--- bin/www.js
+++ bin/www.js
@@ -9,7 +9,6 @@ import debug from 'debug';
 const debugApp = new debug('learnwebrtc:server');
 import http from 'http';
 import { Server } from 'socket.io';
-
 /**
  * Get port from environment and store in Express.
  */
@@ -20,8 +19,8 @@ app.set('port', port);
 /**
  * Create HTTP server.
  */
-
 const server = http.Server(app);
+
 /**
  * Listen on provided port, on all network interfaces.
  */
@@ -29,11 +28,34 @@ const server = http.Server(app);
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
+/**
+ * Config socket.io
+ */
 
+const arrUserInfo = [];
 const io = new Server(server);
 io.on('connection', (socket) => {
-	console.log(socket.id);
+	socket.on('NGUOI_DUNG_DANG_KY', (user) => {
+		const isExist = arrUserInfo.some(
+			(userInfo) => userInfo === user.username,
+		);
+		if (isExist) {
+			return socket.emit('DANG_KY_THAT_BAI');
+		}
+		socket.peerId = user.id;
+		arrUserInfo.push(user);
+		socket.emit('DANH_SACH_ONLINE', arrUserInfo);
+		socket.broadcast.emit('CO_NGUOI_DUNG_MOI', user);
+	});
+	socket.on('disconnect', () => {
+		const index = arrUserInfo.findIndex(
+			(user) => user.id === socket.peerId,
+		);
+		arrUserInfo.splice(index, 1);
+		io.emit('AI_DO_NGAT_KET_NOI', socket.peerId);
+	});
 });
+
 /**
  * Normalize a port into a number, string, or false.
  */
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,21 @@
-const socket = io('http://localhost:3000');
+const socket = io('/');
+$('#chat').hide();
+socket.on('DANH_SACH_ONLINE', (arrUserInfo) => {
+	$('#chat').show();
+	$('#signup').hide();
+	arrUserInfo.forEach((user) => {
+		const { username, id } = user;
+		$('#ulUsers').append(`<li id="${id}">${username}</li>`);
+	});
+	socket.on('CO_NGUOI_DUNG_MOI', (user) => {
+		const { username, id } = user;
+		$('#ulUsers').append(`<li id="${id}">${username}</li>`);
+	});
+	socket.on('AI_DO_NGAT_KET_NOI', (peerId) => {
+		$(`#${peerId}`).remove();
+	});
+});
+socket.on('DANG_KY_THAT_BAI', () => alert('Vui long chon username khac'));
 function openStream() {
 	const config = { audio: false, video: true };
 	return navigator.mediaDevices.getUserMedia(config);
@@ -9,7 +26,13 @@ function playStream(idVideoTag, stream) {
 	video.play();
 }
 const peer = new Peer();
-peer.on('open', (id) => $('#my-peer').append(id));
+peer.on('open', (id) => {
+	$('#my-peer').append(id);
+	$('#btnSignup').click(() => {
+		const username = $('#txtUsername').val();
+		socket.emit('NGUOI_DUNG_DANG_KY', { username, id });
+	});
+});
 $('#btnCall').click(() => {
 	const id = $('#remoteId').val();
 	openStream().then((stream) => {
@@ -24,6 +47,16 @@ peer.on('call', (call) => {
 	openStream().then((stream) => {
 		call.answer(stream);
 		playStream('localStream', stream);
+		call.on('stream', (remoteStream) =>
+			playStream('remoteStream', remoteStream),
+		);
+	});
+});
+$('#ulUsers').on('click', 'li', function () {
+	const id = $(this).attr('id');
+	openStream().then((stream) => {
+		playStream('localStream', stream);
+		const call = peer.call(id, stream);
 		call.on('stream', (remoteStream) =>
 			playStream('remoteStream', remoteStream),
 		);
